98388f1acd32df18d129fda7afb1137edf740a6b
js-react-developer <slava.lyutkevich8308@gmail.com>
2021-03-09 21:47:24 +0300

    first use of NavLink lesson 20

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
-import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header';
-import Music from './components/Music/Music';
 import Navbar from './components/Navbar/Navbar';
-import News from './components/News/News';
+import Dialogs from './components/Dialogs/Dialogs';
 import Profile from './components/Profile/Profile';
+import News from './components/News/News';
+import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
 
 const App = (props) => {
