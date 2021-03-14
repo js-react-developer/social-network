3e7b6d3bb95a60968706b78533d4d4bfb4358319
js-react-developer <slava.lyutkevich8308@gmail.com>
2021-03-14 15:25:22 +0300

    implementation button 'add post', FLUX concept | lesson 33

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import state, { addPost } from './redux/state'
-import App from './App';
+import state from './redux/state'
 import reportWebVitals from './reportWebVitals';
+import { rerenderEntireTree } from './render';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App state={state} addPost={addPost}/>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+rerenderEntireTree(state);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
