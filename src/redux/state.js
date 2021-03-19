e728d515b44308fbac1036c289329f33a2694c42
js-react-developer <slava.lyutkevich8308@gmail.com>
2021-03-20 00:48:22 +0300

    FLUX concept for each char in the textarea | lesson 34

--- src/redux/state.js
+++ src/redux/state.js
@@ -7,7 +7,8 @@ let state = {
             { id: 2, message: "It's my second message", likesCount: 20 },
             { id: 3, message: "It's my third message", likesCount: 30 },
             { id: 4, message: "It's my fourth message", likesCount: 40 }
-        ]
+        ],
+        newPostText: 'hi'
     },
 
     dialogsPage: {
@@ -37,13 +38,19 @@ let state = {
     }
 }
 
-export let addPost = (postMessage) => {
+export let addPost = () => {
     let newPost = {
         id: 5,
-        message: postMessage,
+        message: state.profilePage.newPostText,
         likesCount: 0
     };
     state.profilePage.posts.push(newPost);
+    state.profilePage.newPostText = '';
+    rerenderEntireTree(state);
+}
+
+export let updateNewPostText = (newText) => {
+    state.profilePage.newPostText = newText;
     rerenderEntireTree(state);
 }
 
