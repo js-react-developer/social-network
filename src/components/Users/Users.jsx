8147a809784a6351e5c0d3167c3bbcf54ba13e59
js-react-developer <slava.lyutkevich8308@gmail.com>
2021-04-08 23:31:22 +0300

    using class component Users, life cycle methods: componentDidMount | lesson 50-54

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,43 +1,53 @@
+import axios from 'axios';
 import React from 'react';
 import s from './Users.module.css';
+import userPhoto from '../../assets/images/user.png'
 
-const Users = (props) => {
-    return (
-        <div>
+class Users extends React.Component {
+
+    componentDidMount() {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {
+                this.props.setUsers(response.data.items);
+            });
+    }
+
+    render() {
+        return <div>
             {
-                props.users.map(u => <div>
+                this.props.users.map(u => <div>
                     <span>
                         <div>
-                            <img src={u.photoUrl} className={s.photoUser} />
+                            <img src={u.photos.small != null ? u.photos.small : userPhoto} className={s.photoUser} />
                         </div>
                         <div>
-                            { u.followed
-                            ? <button onClick={ () => { props.unfollow(u.id)} }>Unfollow</button>
-                            : <button onClick={ () => { props.follow(u.id)} }>Follow</button>}
+                            {u.followed
+                                ? <button onClick={() => { this.props.unfollow(u.id) }}>Unfollow</button>
+                                : <button onClick={() => { this.props.follow(u.id) }}>Follow</button>}
                         </div>
                     </span>
                     <span>
                         <span>
                             <div>
-                                { u.fullName }
+                                {u.name}
                             </div>
                             <div>
-                                { u.status }
+                                {u.status}
                             </div>
                         </span>
                         <span>
                             <div>
-                                { u.location.country }
+                                {/* {u.location.country} */}
                             </div>
                             <div>
-                                { u.location.city}
+                                {/* {u.location.city} */}
                             </div>
                         </span>
                     </span>
                 </div>)
             }
         </div>
-    )
+    }
 
 }
 
--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -4,10 +4,7 @@ const SET_USERS = 'SET_USERS';
 
 let initialState = {
     users: [
-        {id: 1, photoUrl: 'https://pbs.twimg.com/profile_images/1105124188476579840/vAo_ZPMt_400x400.jpg', followed: true, fullName: 'Dmitry', status: 'I am a boss', location: {city: 'Minsk', country: 'Belarus'} },
-        {id: 2, photoUrl: 'https://pbs.twimg.com/profile_images/1105124188476579840/vAo_ZPMt_400x400.jpg', followed: false, fullName: 'Alexander', status: 'I am a boss too', location: {city: 'Moscow', country: 'Russia'} },
-        {id: 3, photoUrl: 'https://pbs.twimg.com/profile_images/1105124188476579840/vAo_ZPMt_400x400.jpg', followed: true, fullName: 'Andrew', status: 'I am a boss too', location: {city: 'Kiev', country: 'Ukraine'} },
-        {id: 4, photoUrl: 'https://pbs.twimg.com/profile_images/1105124188476579840/vAo_ZPMt_400x400.jpg', followed: false, fullName: 'Yana', status: 'I am a boss too', location: {city: 'Warszawa', country: 'Poland'} }
+    
     ]
 };
 
